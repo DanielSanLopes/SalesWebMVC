0103f44a1c1d2f571baa022b2fb56b257b749766
Daniel-San <danielsan_lopes@outlook.com>
2024-06-20 01:53:46 -0300

    First Model-View-Controller - Department

--- SalesWebMVC/SalesWebMVC/obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
+++ SalesWebMVC/SalesWebMVC/obj/Debug/net8.0/SalesWebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f4346b7c333eb5634fbb5a94c0dfa8d8be332c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18053d273ac9fc2eafb148432169c07c2dd64b39")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
