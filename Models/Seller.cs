03a86d1a87df73221475e1cb2b3f29cb571e8703
DanielSanLopes <DanielSan_Lopes@outlook.com>
2024-12-27 01:24:48 -0300

    DepartmentForeingKey

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -9,6 +9,7 @@ namespace SalesWebMVC.Models {
         public float BaseSalary { get; set; }
         public DateTime BirthDate { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
